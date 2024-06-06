247edd88eb9242e9a9dcc7d9296d7e9c6fdc0ab3
radzp <anne.paczesna@gmail.com>
2024-06-06 16:49:51 +0200

    Initial commit

---
+++ src/main/java/com/amw/datawave/DataWaveApplication.java
@@ -0,0 +1,13 @@
+package com.amw.datawave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataWaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataWaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amw/datawave/DataWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amw.datawave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataWaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
